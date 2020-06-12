ebf281ac08ba53317eb70fab13ad87becf43083c
kamranhossain <kamran.hossain227@gmail.com>
2020-06-12 20:25:29 +0600

    add js function for concat all js file and gulp task for js

--- gulp-task-example1/gulpfile.js
+++ gulp-task-example1/gulpfile.js
@@ -3,3 +3,14 @@ const concat = require("gulp-concat");
 const json = require("./js.json");
 const uglify = require("gulp-uglify");
 
+function js(done) {
+  gulp
+    .src(json.files)
+    .pipe(concat("all.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("static/js"));
+  done();
+}
+
+gulp.task("js", js);
+
